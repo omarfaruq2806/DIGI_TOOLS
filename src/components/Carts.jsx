ec7d0943a95c5e75e4cb5c46c7034132906e2f86
omarfaruk <mdomarfaruq2806@gmail.com>
2026-04-01 21:42:31 +0600

    UI updated for Carts

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -17,7 +17,8 @@ const Carts = ({ carts, setCarts }) => {
   };
 
   return (
-    <div>
+    <div className="px-3">
+      <h1 className="text-2xl font-bold mb-4 py-6">Your Cart</h1>
       {carts.length === 0 ? (
         <div className="bg-gray-100 text-center mt-10  py-20 px-10 rounded-2xl shadow-xs space-y-4">
           <h2 className="text-2xl font-bold">Your cart is empty</h2>
@@ -26,23 +27,23 @@ const Carts = ({ carts, setCarts }) => {
           </p>
         </div>
       ) : (
-        <div className="space-y-4 my-4 p-8 border border-gray-200 rounded-xl shadow-xs">
+        <div className="space-y-4 my-4 p-3 md:p-8 border border-gray-200 rounded-xl shadow-xs">
           {carts.map((item) => {
             return (
               <div
                 key={item.id}
-                className="border border-gray-100 shadow-xs rounded-xl  p-6 space-y-4 flex items-center justify-between"
+                className="border border-gray-100 shadow-xs rounded-xl p-1 md:p-4 space-y-4 flex items-center justify-between"
               >
                 <div className="flex gap-3 items-center">
                   <div>
-                    <img src={item.icon} alt="logo" className="p-2 w-18 bg-gray-50 rounded-2xl"/>
+                    <img src={item.icon} alt="logo" className=" w-6 md:w-18  bg-gray-50 rounded-2xl"/>
                   </div>
                   <div className="flex flex-col text-left">
-                    <h1 className="text-xl font-medium">{item.name}</h1>
+                    <h1 className="text-sm md:text-xl font-medium">{item.name}</h1>
                     <p>${item.price}</p>
                   </div>
                 </div>
-                <button onClick={() => removeFromCart(item.id , item.name)} className="text-red-400 ">Remove</button>
+                <button onClick={() => removeFromCart(item.id , item.name)} className="text-red-400 text-sm md:text-lg">Remove</button>
               </div>
             );
           })}
